acf74a05fbf648a9b4fb4f49a6cdac931f0399e9
James Stamps Jr <jstampshere@gmail.com>
2020-02-15 14:27:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -75,5 +75,11 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  
+  if (cardNumber){
+    let totalCost = total();
+    cart =[];
+    return`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
+  } else {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
 }
